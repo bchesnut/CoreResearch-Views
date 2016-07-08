2d355fb663eb14e93c63d1f3636aa86c564918cb
bchesnut <blair.chesnut@gmail.com>
2016-07-08 18:03:23 -0400

    Project restriction

--- v_duke_restrict_project.sql
+++ v_duke_restrict_project.sql
@@ -5,7 +5,7 @@ select p.s_projectid,
        sysuser u
          inner join jobtype jt on jt.jobtypeid = u.lastjobtype
  where (
-         (jt.u_jobtypeclass = 'PI' AND u.sysuserid = p.u_pi)
+         (jt.u_jobtypeclass in ('PI', 'EXTERNAL_FACILITY_USER') AND u.sysuserid = p.u_pi)
       or (jt.u_jobtypeclass = 'PI_DELEGATE' and p.u_pi in (select pi from u_pisubmitter ps where ps.submitter = u.sysuserid and nvl(ps.proxytopiflag, 'N') = 'Y'))
       or (jt.u_jobtypeclass in ('FACILITY_USER', 'EXTERNAL_FACILITY_USER') and p.s_projectid in (select distinct s_projectid from s_projectmember pm where pm.sysuserid = u.sysuserid))
       or (jt.u_jobtypeclass = 'FINANCIAL_MANAGER' AND exists (select 1 from u_fundsource fs where fs.u_fundsourceid = p.u_fundsourceid and (fs.fm1 = u.sysuserid or fs.fm2 = u.sysuserid)))
